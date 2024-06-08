e15d212a69bc76b564f3252c4d3dc62c3d55f041
David J. Bradshaw <dave@bradshaw.net>
2024-06-08 14:55:50 +0100

    project setup

---
+++ rollup.config.mjs
@@ -0,0 +1,21 @@
+import filesize from "rollup-plugin-filesize";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "index.js",
+    format: "esm",
+    name: "ifrModal",
+  },
+  plugins: [
+    filesize(),
+    terser({
+      output: {
+        comments: false,
+      },
+    }),
+    resolve(),
+  ],
+}
